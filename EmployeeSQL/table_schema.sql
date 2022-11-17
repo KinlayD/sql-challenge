64d38f85e10f7157a8383751f4f118e71dd212c4
KinlayDenning <89169906+KinlayD@users.noreply.github.com>
2022-11-17 17:18:02 +1100

    table schema

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -1,3 +1,49 @@
--- drop tables 
+-- drop table if exists 
+DROP TABLE titles;
+DROP TABLE employees;
+DROP TABLE departments;
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE salaries;
 
--- create tables
+
+-------------------
+-- create tables -- 
+-------------------
+
+CREATE TABLE titles (
+    title_id VARCHAR PRIMARY KEY,
+    title VARCHAR
+);
+
+CREATE TABLE employees (
+ 	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR REFERENCES titles (title_id),
+	birth_date DATE,
+	first_name VARCHAR,
+    last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE			
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT REFERENCES employees (emp_no),
+    dept_no VARCHAR REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no,dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR REFERENCES departments (dept_no),
+    emp_no INT REFERENCES employees (emp_no),
+    PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INT REFERENCES employees (emp_no),
+    salary INT 
+);
